c8068b0af145b200f03c25464c44ad51a9bd3aab
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-18 19:19:01 -0600

    firs commit

---
+++ 01_output.php
@@ -0,0 +1,25 @@
+<?php
+echo "hello world";
+
+echo 123;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+ <h1>
+  <?php echo 'Post One' ?>
+ </h1>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo 'Hello';
+echo 'World';
