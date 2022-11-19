81c859318635b7c711300f32052ed17a83f687fd
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-18 19:55:22 -0600

    added loops

--- 05_loops.php
+++ 05_loops.php
@@ -11,6 +11,10 @@
   }
 */
 
+for ($i = 0; $i <= 10; $i++) { 
+  echo $i;
+}
+
 
 /* ------------ While Loop ------------ */
 
@@ -32,7 +36,11 @@
 
 do...while loop will always execute the block of code once, even if the condition is false.
 */
-
+$a = 1;
+while ($a <= 10) {
+  echo "<div>Number: ${a}</div>";
+  $a++;
+}
 
 
 /* ---------- Foreach Loop ---------- */
@@ -43,3 +51,32 @@ do...while loop will always execute the block of code once, even if the conditio
   // code to be executed
   }
 */
+
+$posts = [
+  'first post',
+  'second post',
+  'third post'
+];
+
+// for ($i=0; $i < count($posts); $i++) { 
+//   echo "<div>$posts[$i]</div>";
+// }
+
+foreach ($posts as $post) {
+  echo "<div>
+    {$post}
+  </div>";
+}
+
+$person = [
+  'first_name' => 'Brad',
+  'last_name' => 'Traversy'
+];
+
+foreach ($person as $key => $value) {
+  echo "$key - $value <br/>";
+};
+
+
+
+
