8f28ba2b4d52c7e6be93457f24f265541e681c7a
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-18 19:35:52 -0600

    added associative arrays

--- 03_arrays.php
+++ 03_arrays.php
@@ -4,3 +4,34 @@
 /*
   Arrays hold multiple values. Each value in an array is called an "element"
 */
+$numbers = [1, 33, 232, 3432, 234];
+
+// print arrays
+//print_r($numbers);
+//echo $numbers[1];
+
+
+// Associative array
+
+$colors = [
+  1 => 'red',
+  4 => 'blue',
+  5 => 'green'
+];
+
+//echo $colors[4];
+
+$hex = [
+  'red' => '#f00',
+  'blue' => '#0f0'
+];
+
+echo $hex['red'];
+
+$person = [
+  'first_name' => 'Brad',
+  'last_name' => 'Traversy',
+  'email' => 'Brad@gmail.com'
+];
+
+echo $person['first_name'];
