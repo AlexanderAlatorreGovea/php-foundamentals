a0db8a59df46159edd9dbfc1bdc26a11dece52b5
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-20 20:53:27 -0600

    added cookies

--- 12_cookies.php
+++ 12_cookies.php
@@ -4,3 +4,11 @@
 /*
   Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
 */
+
+//set cookie
+
+setcookie('name', 'brad', time() + 86400);
+
+if (isset($_COOKIE['name'])) {
+  echo $_COOKIE['name'];
+};
--- 13_sessions.php
+++ 13_sessions.php
@@ -5,3 +5,4 @@
   Sessions are a way to store information (in variables) to be used across multiple pages.
   Unlike cookies, sessions are stored on the server.
 */
+
