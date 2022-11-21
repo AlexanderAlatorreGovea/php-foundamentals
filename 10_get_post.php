b77c2d65a1d6dc53f73f0c367e59e30b29d12632
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-20 20:42:38 -0600

    added post request

--- 10_get_post.php
+++ 10_get_post.php
@@ -4,3 +4,33 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+if (isset($_POST['submit'])) {
+
+  echo $_GET['name']; 
+  echo $_GET['age'];
+};
+
+?>
+
+<a href="
+  <?php echo $_SERVER['PHP_SELF']; ?>
+  ?name=John
+  &age=40">
+  link
+</a>
+
+<br><br>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <div>
+    <label>Name: </label>
+    <input type="text" name="name">
+  </div>
+  <br>
+  <div>
+    <label>Password: </label>
+    <input type="password" name="password">
+  </div>
+  <br>
+  <input type="submit" name="submit" value="Submit">
+</form>
