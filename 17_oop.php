9de01955dab6a66d0f77105ee98499285ccb5026
Alexander <Alexander_alatorre1993@hotmail.com>
2022-11-21 18:46:01 -0600

    added oop class

--- 17_oop.php
+++ 17_oop.php
@@ -1,6 +1,44 @@
-<?php 
+<?php
 /* --- Object Oriented Programming -- */
 
 /*
   From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
 */
+
+class User
+{
+  // public - can be accessed from anywhere
+  // private - can only be accessed from inside the class
+  // protected - can only be accessed from inside the class and by inheriting classes
+  public $name;
+  private $email;
+  public $password;
+
+  public function __construct($name, $email, $password)
+  {
+    $this->name = $name;
+    $this->email = $email;
+    $this->password = $password;
+  }
+
+  function getName()
+  {
+    return $this->name;
+  }
+
+  function login()
+  {
+    return "User $this->name is logged in.";
+  }
+
+  // Destructor is called when an object is destroyed or the end of the script.
+  function __destruct()
+  {
+    echo "The user name is {$this->name}";
+  }
+}
+
+$user = new User("Alex", "alex@gmail.com", 'safsfdivsda11');
+$user->name = 'Brad';
+
+var_dump($user);
